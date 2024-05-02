407fada740280c8b1560acc45f659427bd026f51
Evan Wu <yifan.wu.y@gmail.com>
2024-05-02 22:15:40 +0800

    fix

--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -22,64 +22,68 @@ function TransactionsPage() {
     to: new Date(),
   });
   return (
-    <div className="border-b bg-card">
-      <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-        <div>
-          <p className="text-3xl font-bold">Transactions history</p>
-        </div>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              id="date"
-              variant={"outline"}
-              className={cn(
-                "w-[300px] justify-start text-left font-normal",
-                !dateRange && "text-muted-foreground",
-              )}
-            >
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              {dateRange?.from ? (
-                dateRange.to ? (
-                  <>
-                    {format(dateRange.from, "LLL dd, y")} -{" "}
-                    {format(dateRange.to, "LLL dd, y")}
-                  </>
+    <>
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <div>
+            <p className="text-3xl font-bold">Transactions history</p>
+          </div>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                id="date"
+                variant={"outline"}
+                className={cn(
+                  "w-[300px] justify-start text-left font-normal",
+                  !dateRange && "text-muted-foreground",
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {dateRange?.from ? (
+                  dateRange.to ? (
+                    <>
+                      {format(dateRange.from, "LLL dd, y")} -{" "}
+                      {format(dateRange.to, "LLL dd, y")}
+                    </>
+                  ) : (
+                    format(dateRange.from, "LLL dd, y")
+                  )
                 ) : (
-                  format(dateRange.from, "LLL dd, y")
-                )
-              ) : (
-                <span>Pick a date</span>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0" align="start">
-            <Calendar
-              initialFocus
-              mode="range"
-              numberOfMonths={2}
-              defaultMonth={dateRange?.from}
-              selected={dateRange}
-              onSelect={(v) => {
-                console.log(v);
-                if (!v) {
-                  return;
-                }
+                  <span>Pick a date</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                initialFocus
+                mode="range"
+                numberOfMonths={2}
+                defaultMonth={dateRange?.from}
+                selected={dateRange}
+                onSelect={(v) => {
+                  console.log(v);
+                  if (!v) {
+                    return;
+                  }
 
-                if (differenceInDays(v.to!, v.from!) > MAX_DATE_RANGE_DAYS) {
-                  toast.error(`Max date range is ${MAX_DATE_RANGE_DAYS} days`);
-                  return;
-                }
+                  if (differenceInDays(v.to!, v.from!) > MAX_DATE_RANGE_DAYS) {
+                    toast.error(
+                      `Max date range is ${MAX_DATE_RANGE_DAYS} days`,
+                    );
+                    return;
+                  }
 
-                setDateRange(v);
-              }}
-            />
-          </PopoverContent>
-        </Popover>
+                  setDateRange(v);
+                }}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
       <div className="container">
         <TransactionTable from={dateRange.from!} to={dateRange.to!} />
       </div>
-    </div>
+    </>
   );
 }
 
