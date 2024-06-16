f29c4797b2619f14f94a47b9fb7f86ea3767bd3b
Evan Wu <yifan.wu.y@gmail.com>
2024-06-16 21:11:36 +0800

    navbar border

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const items = [
 function MobileNavbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="block border-separate bg-background md:hidden">
+    <div className="block border-separate border-b bg-background md:hidden">
       <nav className="container flex items-center justify-between px-8">
         <Sheet open={isOpen} onOpenChange={setIsOpen}>
           <SheetTrigger asChild>
